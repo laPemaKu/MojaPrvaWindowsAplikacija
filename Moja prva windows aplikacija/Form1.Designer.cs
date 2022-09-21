649235d125fbb71464de87bf857546ced5477813
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2022-09-21 09:32:54 +0200

    Add files via upload

---
+++ Moja prva windows aplikacija/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Moja_prva_windows_aplikacija
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUnosBrojaA = new System.Windows.Forms.TextBox();
+            this.txtUnosBrojaB = new System.Windows.Forms.TextBox();
+            this.txtRez = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnIzracun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUnosBrojaA
+            // 
+            this.txtUnosBrojaA.Location = new System.Drawing.Point(12, 38);
+            this.txtUnosBrojaA.Name = "txtUnosBrojaA";
+            this.txtUnosBrojaA.Size = new System.Drawing.Size(100, 20);
+            this.txtUnosBrojaA.TabIndex = 0;
+            // 
+            // txtUnosBrojaB
+            // 
+            this.txtUnosBrojaB.Location = new System.Drawing.Point(358, 38);
+            this.txtUnosBrojaB.Name = "txtUnosBrojaB";
+            this.txtUnosBrojaB.Size = new System.Drawing.Size(100, 20);
+            this.txtUnosBrojaB.TabIndex = 1;
+            // 
+            // txtRez
+            // 
+            this.txtRez.Enabled = false;
+            this.txtRez.Location = new System.Drawing.Point(12, 222);
+            this.txtRez.Name = "txtRez";
+            this.txtRez.ReadOnly = true;
+            this.txtRez.Size = new System.Drawing.Size(100, 20);
+            this.txtRez.TabIndex = 6;
+            this.txtRez.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Broj A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(358, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Broj B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Zbroj";
+            // 
+            // btnIzracun
+            // 
+            this.btnIzracun.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIzracun.Location = new System.Drawing.Point(361, 222);
+            this.btnIzracun.Name = "btnIzracun";
+            this.btnIzracun.Size = new System.Drawing.Size(100, 23);
+            this.btnIzracun.TabIndex = 2;
+            this.btnIzracun.Text = "Izračunaj";
+            this.btnIzracun.UseVisualStyleBackColor = true;
+            this.btnIzracun.Click += new System.EventHandler(this.btnIzracun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIzracun);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRez);
+            this.Controls.Add(this.txtUnosBrojaB);
+            this.Controls.Add(this.txtUnosBrojaA);
+            this.Name = "Form1";
+            this.Text = "Moja prva windows aplikacija";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUnosBrojaA;
+        private System.Windows.Forms.TextBox txtUnosBrojaB;
+        private System.Windows.Forms.TextBox txtRez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIzracun;
+    }
+}
+
---
+++ Moja prva windows aplikacija/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moja_prva_windows_aplikacija
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIzracun_Click(object sender, EventArgs e)
+        {
+            int brojA, brojB, zbroj;
+            brojA = Convert.ToInt32(txtUnosBrojaA.Text);
+            brojB = Convert.ToInt32(txtUnosBrojaB.Text);
+            zbroj = brojA + brojB;
+
+            txtRez.Text =Convert.ToString(zbroj);
+        }
+    }
+}
